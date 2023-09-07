77521a42c95442608d3d26633107c1d97b944720
Carlos Napoles Avila <napolesmoa1006@gmail.com>
2023-09-07 15:28:58 -0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿
+using HtmlAgilityPack;
+using System.Xml;
+
+const string SiteUrl = "https://bing.gifposter.com/";
+const string DestinationFolder = "C:\\Wallpaper\\";
+
+if (!Directory.Exists(DestinationFolder)) Directory.CreateDirectory(DestinationFolder);
+
+// Get Html
+var client = new HttpClient
+{
+    Timeout = TimeSpan.FromMilliseconds(Timeout.Infinite)
+};
+
+string htmlBody = await client.GetStringAsync(SiteUrl + "sitemap.xml");
+
+// From String
+var xml = new XmlDocument();
+xml.LoadXml(htmlBody);
+
+XmlNodeList xmlNodes = xml.GetElementsByTagName("url");
+
+foreach (XmlNode node in xmlNodes)
+{
+    if (node["loc"] == null) continue;
+
+    string url = node["loc"]!.InnerText;
+
+    if (url.StartsWith(SiteUrl + "wallpaper-") && url.EndsWith(".html"))
+    {
+        string html = await client.GetStringAsync(url);
+
+        // From String
+        var doc = new HtmlDocument();
+        doc.LoadHtml(html);
+        
+        HtmlNode htmlNode = doc.DocumentNode.SelectSingleNode("//a[@class='icon download']");
+
+        string link = htmlNode.Attributes["href"].Value;
+        string downloadLink = SiteUrl + link;
+
+        // You have to create the folder
+        string destinationFile = DestinationFolder + link.Replace("/bingImages/", "");
+
+        if (File.Exists(destinationFile)) continue;
+
+        var uri = new Uri(downloadLink);
+
+        await client.DownloadFileTaskAsync(uri, destinationFile);
+        Console.WriteLine("File downloaded... '{0}'", downloadLink);
+    }
+}
+
+public static class HttpClientExtension
+{
+    public static async Task DownloadFileTaskAsync(this HttpClient client, Uri uri, string fileName)
+    {
+        Stream stream = await client.GetStreamAsync(uri);
+        
+        var fileStream = new FileStream(fileName, FileMode.CreateNew);
+        
+        await stream.CopyToAsync(fileStream);
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Download Bing Wallpapers
+
+I have always liked Bing Wallpapers and I discovered that they are all available at https://bing.gifposter.com/ but downloading them manually was a daunting task.
+Until I discovered WebScraping and everything became easier. This is a small console app that allows you to download most of the Bing wallpapers.
+I hope you enjoy it as I do
